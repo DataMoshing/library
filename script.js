fe155b701cd45e07c1bac204d94f0bb96779ec0b
DataMoshing <imkiloss@gmail.com>
2022-08-14 20:46:09 -0600

    Creates a table dynamically with user input

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
-const author = document.getElementById("author").innerText
-const title = document.getElementById("title").innerText
+const libraryForm = document.querySelector("form");
+const btn = document.querySelector("button");
+const table = document.querySelector("#table");
 
-
-// Books will be stored into myLibrary array
-let myLibrary = [];
+myLibrary = [];
 
 // Constructor function for books
-function Book(title, author, read) {
-    this.title = title;
+function Book(author, title, read, pages) {
     this.author = author;
+    this.title = title;
     this.read = read;
+    this.pages = pages
 }
 
-const addBook = new Book({ author, title })
-
-//Add or push books into library array
-function addBookToLibrary() {
+//Create instance of object then push to myLibrary array
+function addBookToLibrary(event) {
+    event.preventDefault();
+    title = title.value;
+    author = author.value;
+    read = read.value;
+    pages = pages.value;
+    const addBook = new Book(author, title, read, pages)
     myLibrary.push(addBook)
+    displayBook()
+    console.log(addBook)
+    return addBook
 }
+
+//Test
+const testBook = new Book("Maurice Sendak", "Where the Wild Things Are", "Read")
+
+// Create book when submit button is clicked
+libraryForm.addEventListener("submit", addBookToLibrary)
+
+
+// Loop over myLibrary array then display
+function displayBook() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let html = "<table border= '1|1'>";
+        html += "<tr>";
+        html += "<th>" + "Author" + "</th>"
+        html += "<td>" + myLibrary[i].author + "</td>";
+        html += "</tr>";
+        html += "<th>" + "Title" + "</th>"
+        html += "<td>" + myLibrary[i].title + "</td>";
+        html += "<tr>";
+        html += "<th>" + "Read" + "</th>"
+        html += "<td>" + myLibrary[i].read + "</td>";
+        html += "</tr>";
+        html += "<tr>";
+        html += "<th>" + "Pages" + "</th>"
+        html += "<td>" + myLibrary[i].pages + "</td>";
+        html += "</tr>";
+        html += "</table>";
+        table.innerHTML = html;
+    }
+}
+
